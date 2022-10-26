27fe545be84cde8d6ddcf838525c991f23767234
shrukGIT <shahrukhaa00@gmail.com>
2022-10-27 02:16:33 +0530

    Scrrens were added and Some quick starter changes were made

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { Button,  StatusBar,StyleSheet, Switch, Text, View } from 'react-native';
+import Sc1 from "@Screens/screen1"
+import Sc2 from "@Screens/screen2"
+import { useRef } from 'react';
 
 export default function App() {
+  const value = useRef(false);
+
+
+  const hidebar=()=>{
+    value.current=!value.current
+    StatusBar.setHidden(value.current,"slide");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Sc1/>
+      <Sc2/>
+      <Button title='clickme' onPress={hidebar} />
+      <StatusBar translucent={true}  style={styles.titleBar} backgroundColor='red' />
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titleBar:{
+
+
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
---
+++ Screens/screen2/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function index() {
+  return (
+    <Text>Hello screen 1</Text>
+  )
+}
+
+export default index
